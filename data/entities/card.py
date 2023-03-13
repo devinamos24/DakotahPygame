a7b11876f69864f2ae155548e98c3abfed9e2913
DakotahPro <dakotah.proffit@gmail.com>
2023-03-13 08:48:21 -0500

    -- Fixed movement validator iteration

--- data/entities/card.py
+++ data/entities/card.py
@@ -6,48 +6,6 @@ from data.entities.action import _Action
 from data.entities.actor import _Actor
 
 
-# This will be what used to write the validating functaions for the diffrent result types
-# class validator:
-#     def __init__(self, arg):
-#         pass
-#
-#     def location_validator():
-#         pass
-#
-#
-# # The result types are what allow the cards to do actions
-# class Result:
-#     def __init__(self, move_loc=None, damage_loc=None, stage_mod_loc=None, summon_loc=None, debuff_loc=None,
-#                  buff_loc=None):
-#         pass
-#
-#     def movement(self, valid_movement):
-#         # store valid move, damage, summon, stage_mod and rules for use
-#         # check validity of result through custom fuctions and rules of use
-#         # follow through
-#         pass
-#
-#     def damage(self, valid_damage):
-#         pass
-#
-#     def stage_mod(self, valid_stage_mod):
-#         pass
-#
-#     def summon(self, valid_summon):
-#         pass
-#
-#     def debuff(self, valid_debuff):
-#         pass
-#
-#     def buff(self, valid_buff):
-#         pass
-#
-#
-# # card class that will list the cards types, text description of what the card will do, and the cards "result" or calculated action
-# class _card:
-#     def __init__(self, card_type: str, description: str, result: Result):
-#         pass
-
 class Indicator:
     def __init__(self, x, y, indicator_type, action):
         self.x = x
@@ -94,8 +52,12 @@ class _Card:
                                     return False
                             return True
                         if check_and_set():
-                            return check_x, check_y
-                        return None
+                            result = True
+                        else:
+                            break
+                    else:
+                        return check_x, check_y
+                    return None
 
                 return check_or_position()
         except Exception:
@@ -118,7 +80,9 @@ class RookCard(_Card):
     def activate(self, indicator_list):
         def move(new_x, new_y): return lambda: (self.owner.move(new_x, new_y))
         moves = []
+        moves.append(Position((0, 1), ([(0, 1)], [(0, 1)]))) #python doesnt like only having 1 position check and simplifies it and messes with the data structure
         moves.append(Position((0, 2), ([(0, 1)], [(0, 2)])))
+        moves.append(Position((0, 3), ([(0, 1)], [(0, 2)], [(0, 3)])))
         if self.owner is not None:
             for _move in moves:
                 valid_move = self._validate(_move)
